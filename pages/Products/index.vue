13f234d98588a8df8eba2c343802977416211c42
Fah1m09 <mahmudurfahim@gmail.com>
2025-01-13 17:58:52 +0600

    feat: product list ui

--- pages/Products/index.vue
+++ pages/Products/index.vue
@@ -1,1 +1,34 @@
-<template></template>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useProductStore } from "~/store/product";
+
+const productStore = useProductStore();
+
+const getProductList = async () => {
+    await productStore
+        .getProductsAction()
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err: any) => {
+            console.log(err);
+        });
+};
+
+onMounted(() => {
+    getProductList();
+});
+</script>
+
+<template>
+    <div class="p-[35px]">
+        <div class="grid grid-cols-4 gap-4">
+            <div v-for="product in productStore.getProducts" class="bg-gray-100 p-2 rounded-lg shadow">
+                <img :src="product.images[0]" alt="" class="">
+                <p>{{ product?.title }}</p>
+                <p>{{ product?.price }}</p>
+            </div>
+        </div>
+
+    </div>
+</template>
